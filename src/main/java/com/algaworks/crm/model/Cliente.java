b8abc8da89400d002fa7b58c97d6b08358bea54f
Felipe Menezes <felipe.menezes@supersim.com.br>
2022-07-24 20:27:01 -0300

    Study Java and Spring Boot creating a simple rest api

---
+++ src/main/java/com/algaworks/crm/model/Cliente.java
@@ -0,0 +1,44 @@
+package com.algaworks.crm.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cliente)) return false;
+        Cliente cliente = (Cliente) o;
+        return id.equals(cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
